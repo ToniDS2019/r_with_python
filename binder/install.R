6aca033d19c69aeb28a1d2a6bec5ac3078014c5b
ToniDS2019 <107944064+ToniDS2019@users.noreply.github.com>
2022-06-23 12:00:33 -0400

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,9 +1,9 @@
 ### install regular packages
 
-install.packages("reticulate") # python support in RMarkdown
-install.packages("ggplot2") # for plotting
-install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
-install.packages ("dpylyr", "ggplot2", "reshape2", "tidyverse", "plotly", "stringr')
+# install.packages("reticulate") # python support in RMarkdown
+# install.packages("ggplot2") # for plotting
+# install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+# install.packages ("dpylyr", "ggplot2", "reshape2", "tidyverse", "plotly", "stringr')
 
 
 ### install bioconductor packages
